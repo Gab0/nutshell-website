df035d311a035477f05d06309608ff36d2bdbbe6
samuel d siqueira <samueldsiqueira@gmail.com>
2023-01-10 01:25:33 -0300

    feat: :lipstick: create style folder templates
    create style folder templates and create color themes

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import { Inter } from '@next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+	return (
+		<>
+			<header>
+				<div></div>
+			</header>
+			<main>
+				<div>
+					<section>
+						<h1>Nutshell</h1>
+						<p>Bioinformatics and Software Development</p>
+          </section>
+          <section>
+            <ul>
+              <li> teste </li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </section>
+				</div>
+			</main>
+		</>
+	);
+}
---
+++ styles/theme/typography/typography.ts
@@ -0,0 +1,7 @@
+const typography = {
+  mainFont: {
+    main: 'Roboto'
+  }
+};
+
+export default typography;
