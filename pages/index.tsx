bd2d7f90f1fe67a11c0ee252fbe145a4f4c3eb27
Gabriel Araujo <gabriel_scf@hotmail.com>
2023-01-10 20:33:12 -0300

    Updates.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,44 @@
 import { Inter } from '@next/font/google';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-	return (
-		<>
-			<header>
-				<div></div>
-			</header>
-			<main>
-				<div>
-					<section>
-						<h1>Nutshell</h1>
-						<p>Bioinformatics and Software Development</p>
-          </section>
-          <section>
-            <ul>
-              <li> teste </li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-          </section>
-				</div>
-			</main>
-		</>
-	);
+  return (
+    <>
+      <header>
+        <div></div>
+      </header>
+      <main>
+        <Header></Header>
+      </main>
+    </>
+  );
+}
+
+export function Header() {
+  return (
+    <>
+      <div className="main">
+        <section>
+          <h1>Nutshell</h1>
+          <p>Bioinformatics and Software Development</p>
+        </section>
+        <Navigator></Navigator>
+      </div>
+    </>
+  )
+}
+
+export function Navigator() {
+  return (
+    <>
+      <section>
+            <Link href="/about">About</Link>
+            <Link href="/blog">Blog</Link>
+            <Link href="/projects">Projects</Link>
+      </section>
+    </>
+  )
 }
