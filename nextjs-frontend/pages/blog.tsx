debb089207764e84c2d0bd1ecf287725c63427e3
Gabriel Araujo <gabriel_scf@hotmail.com>
2023-01-20 02:27:41 -0300

    Add the 'blog' page.

---
+++ nextjs-frontend/pages/blog.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/layout/Layout";
+
+import content from "../../content/blog.md";
+
+export default function Project() {
+  return (
+      <Layout content={content} />
+  );
+}
