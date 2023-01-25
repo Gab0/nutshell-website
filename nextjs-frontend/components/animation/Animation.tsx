07341096cb024b2b371eccab16bc0073655e28a6
Gabriel Araujo <gabriel_scf@hotmail.com>
2023-01-25 01:35:14 -0300

    Improve animation.

--- nextjs-frontend/components/animation/Animation.tsx
+++ nextjs-frontend/components/animation/Animation.tsx
@@ -6,34 +6,59 @@ import Canvas from './Canvas';
 import useWindowSize from '@react-hook/window-size';
 
 export default class Biomorphs extends Component {
-  biomorph: Biomorph;
+  biomorphs: Array<Biomorph>;
+  biomorphCount: number;
   width: number;
   height: number;
 
   constructor(props: {}) {
     super(props);
 
+    this.biomorphs = [];
     this.width = 700;
     this.height = 200;
-    this.biomorph = new Biomorph([], this.height);
-    this.biomorph.randomize();
+    this.biomorphCount = 5;
+
+    for (var i = 0; i < this.biomorphCount; i++) {
+      let biomorph = new Biomorph([], this.height);
+      biomorph.randomize();
+      this.biomorphs.push(biomorph);
+    }
+    console.log(this.biomorphs);
   }
 
   componentDidMount() {
   }
 
   drawBall(ctx: CanvasRenderingContext2D, frameCount: number) {
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-      ctx.fillStyle = '#000000';
-      ctx.beginPath();
-      ctx.arc((50 + frameCount) % this.width, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
-      ctx.fill();
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillStyle = '#000000';
+    ctx.beginPath();
+    ctx.arc((50 + frameCount) % this.width, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+    ctx.fill();
   }
 
   drawBiomorphs(ctx: CanvasRenderingContext2D, frameCount: number) {
-    this.biomorph.position = (50 + frameCount) % this.width;
-    if (frameCount % 5 == 0) this.biomorph.mutate();
-    this.biomorph.draw.bind(this.biomorph)(ctx)
+    if (frameCount % 5 == 0) this.reset(ctx);
+
+    for (let i=0; i < this.biomorphCount; i++) {
+      const biomorph = this.biomorphs[i];
+      biomorph.position = (250 * i + frameCount) % this.width;
+      if (frameCount % 5 == 0) biomorph.mutate();
+      biomorph.draw.bind(biomorph)(ctx)
+    }
+
+  }
+
+  reset(ctx: CanvasRenderingContext2D) {
+
+    ctx.clearRect(
+      0,
+      0,
+      this.width,
+      this.height
+    );
+
   }
 
   render() {
--- nextjs-frontend/components/animation/biomorphs.ts
+++ nextjs-frontend/components/animation/biomorphs.ts
@@ -6,11 +6,15 @@
  */
 import random from 'random';
 
+type strokeT = {x1: number, x2: number, y1: number, y2: number};
+type strokesT = Array<strokeT>;
+
 export default class Biomorph {
   width: number;
   height: number;
   position: number;
   genes: Array<number>;
+  strokes: strokesT;
 
   constructor(g: Array<number>, canvasHeight: number) {
     this.genes = g;
@@ -18,6 +22,7 @@ export default class Biomorph {
     this.width = 100;
     this.height = canvasHeight;
     this.position = 0;
+    this.strokes = [];
   }
 
   randomize() {
@@ -37,35 +42,73 @@ export default class Biomorph {
   }
 
   draw(ctx: CanvasRenderingContext2D) {
-    ctx.clearRect(
-      this.position - this.width * 2,
-      0,
-      this.position + this.width * 2,
-      300
-    );
 
     const g = this.genes;
 
     var dx = [-g[1],-g[0],0,g[0],g[1],g[2],0,-g[2]];
     var dy = [g[5],g[4],g[3],g[4],g[5],g[6],g[7],g[6]];
 
-    this.tree(ctx, this.position + (this.width/2), this.height/2, dx, dy, g[8], 2);
+    const strokes: strokesT = this.tree(this.position + (this.width / 2), this.height / 2, dx, dy, g[8], 2);
+
+    ctx.strokeStyle = 'white';
+    this.drawStrokes(ctx, this.strokes);
+
+    this.strokes = strokes;
+
+    ctx.strokeStyle = 'black';
+    this.drawStrokes(ctx, this.strokes);
+
+
   }
 
-  tree(ctx: CanvasRenderingContext2D,
-       x: number, y: number,
+  drawStrokes(ctx: CanvasRenderingContext2D, strokes: strokesT) {
+    for (const s in strokes) {
+      const stroke = strokes[s];
+
+      ctx.beginPath();
+
+      ctx.moveTo(stroke.x1, stroke.y1);
+      ctx.lineTo(stroke.x2, stroke.y2);
+
+      ctx.stroke();
+
+    }
+
+  }
+
+  tree(x: number, y: number,
        dx: Array<number>, dy: Array<number>,
-       len: number, dir: number) {
+       len: number, dir: number): strokesT {
     const x2 = x + (len * dx[dir]);
     const y2 = y + (len * dy[dir]);
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
+
+    let strokes: strokesT = [{
+      x1: x,
+      y1: y,
+      x2: x2,
+      y2: y2
+    }];
+
     if (len > 0) {
-      this.tree(ctx, x2, y2, dx, dy, len - 1, (dir + 7) % 8);
-      this.tree(ctx, x2, y2, dx, dy, len - 1, (dir + 1) % 8);
+      strokes = strokes.concat(this.tree(x2, y2, dx, dy, len - 1, (dir + 7) % 8));
+      strokes = strokes.concat(this.tree(x2, y2, dx, dy, len - 1, (dir + 1) % 8));
+    }
+
+    return strokes;
+  }
+
+  check_bounds(strokes: strokesT) {
+    const minX = 0;
+
+    for (let s in strokes) {
+      const stroke = strokes[s];
+
+      if (stroke.x1) {
+
+      }
+
     }
   }
 
-  }
+}
+
