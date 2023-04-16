e2d0ce3f3666ab8f05b88fb84dcdb8b432b796d3
Gabriel Araujo <gabriel_scf@hotmail.com>
2023-04-15 21:25:15 -0300

    Add footer component.

---
+++ nextjs-frontend/components/footer/Footer.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Footer = () => {
+
+      return (
+        <>
+          <header>
+            <div className="footer">
+              <center>
+              <section className="center">
+                <a href="https://github.com/Gab0">
+                  <Image src="/github-mark.png" width="19" height="19" alt="N" />
+                </a>
+              </section>
+              </center>
+            </div>
+          </header>
+        </>
+      )
+
+}
+
+export default Footer;
