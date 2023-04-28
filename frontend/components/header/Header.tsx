c242a1c12abe8bbbfde857cc9dd47425b671cfc0
Gabriel Araujo <gabriel_scf@hotmail.com>
2023-04-28 20:23:48 -0300

    Fix header and animation misaligment.

--- frontend/components/header/Header.tsx
+++ frontend/components/header/Header.tsx
@@ -4,10 +4,11 @@ import { Inter } from '@next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 const Header = () => {
+  const height = "116px";
       return (
           <>
               <header>
-                  <div className="header">
+                <div className="header" style={{height: height}}>
                       <section className={inter.className}>
                           <h1>nutshell</h1>
                           <p>Bioinformatics and Software Development</p>
